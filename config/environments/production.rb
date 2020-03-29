3c1cc0380e6fbf75d94540f4068cb57d0123bbd9
koheiy1 <59187820+koheiy1@users.noreply.github.com>
2020-03-30 04:13:07 +0900

    comicomi

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -91,4 +92,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
 end
